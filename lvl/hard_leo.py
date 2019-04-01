a0f759d8774cae6ee69b90acb9600ebb67fb98b4
Manoel Vilela <manoel_vilela@engineer.com>
2019-04-01 16:44:33 -0300

    Fix hard_leo.py model

--- lvl/hard_leo.py
+++ lvl/hard_leo.py
@@ -5,23 +5,21 @@ from matplotlib import pyplot as plt
 import math
 
 e = 1
-l_max = 10
-m = -1
+lvl_max = 10
+m = -100
+xp_max = 1000
+xp_factor = 1/(xp_max/50)
+
 
 def lvl(x):
-    return -1/x + l_max
+    return m/(e*(x*xp_factor + lvl_max)) + lvl_max
 
-
-def lvl2(x):
-    return (m - l_max)/(e*x + l_max) + (l_max + 0.1)
-
-
-x = np.linspace(11, 100, 1000)
+x = np.linspace(1, 1000, 1000)
 y = np.vectorize(lambda x: lvl(x))
-y2 = np.vectorize(lvl2)
+# y2 = np.vectorize(lambda x: lvl2(x))
 plt.close('all')
 plt.plot(x, y(x))
-plt.plot(x, y2(x), color='r')
+# plt.plot(x, y2(x), color='r')
 ax = plt.gca()
 ax.set_title("SISTEMA DE LEVEL: HARD LEO")
 ax.set_xlabel("XP")
